3d313c37fd71abb664cded6b5d7ba6de2d1cd6ae
Daniel Zaczek <50755378+dezet98@users.noreply.github.com>
2021-03-20 16:30:50 +0100

    init commit

---
+++ bin/genetic_algorithm.dart
@@ -0,0 +1,5 @@
+import 'package:genetic_algorithm/genetic_algorithm.dart' as genetic_algorithm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${genetic_algorithm.calculate()}!');
+}
---
+++ test/genetic_algorithm_test.dart
@@ -0,0 +1,8 @@
+import 'package:genetic_algorithm/genetic_algorithm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
